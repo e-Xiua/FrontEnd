a3a7233b6509d49a7740da78464faed290c6ca9f
Joaquin Suarez <joaquin.suarez@amwell.com>
2025-12-07 12:58:06 -0500

    change in contact menu to fix whatsapp, telegram and email buttons

--- front-I-Wellness/src/app/shared/ui/components/contact/contact-menu/contact-menu.component.ts
+++ front-I-Wellness/src/app/shared/ui/components/contact/contact-menu/contact-menu.component.ts
@@ -50,14 +50,15 @@ export class ContactMenuComponent implements AfterViewInit {
   }
 
   openWhatsApp(provider: ChatProvider): void {
-    const phoneNumber = provider.telefono || '573132629210';
+    const phoneNumber = provider.proveedorInfo?.telefono || '573132629210';
     const message = `Hola ${provider.contactName}, me interesa conocer más sobre tus servicios de turismo.`;
     const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, '_blank');
   }
 
   openTelegram(provider: ChatProvider): void {
-    const phoneNumber = provider.telefono || '573132629210';
+    console.log(provider)
+    const phoneNumber = provider.proveedorInfo?.telefono || '+573132629210';
     const message = `Hola ${provider.contactName}, me interesa conocer más sobre tus servicios de turismo.`;
     const telegramUrl = `https://t.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(telegramUrl, '_blank');
